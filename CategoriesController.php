4c96c642c187ea7302efb5ded12721dd90c23d47
tranducT2108A <93574589+tranducT2108A@users.noreply.github.com>
2022-08-05 13:27:29 +0700

    Add files via upload

---
+++ CategoriesController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Categories = Categories::latest()->paginate(5);
+        return view('categories.index',compact('Categories'))
+            ->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required',
+        ]);
+
+        categories::create($request->all());
+        return redirect()->route('categories.index')->with('success',
+            'Created Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categories $categories)
+    {
+        return view('categories.show',compact('categories'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categories $categories)
+    {
+        return view('categories.edit',compact('categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categories $categories)
+    {
+        $request->validate([
+            'category_name' => 'required',
+        ]);
+
+        $categories->update($request->all());
+        return redirect()->route('categories.index')->with('success',
+            'Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categories  $categories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categories $categories)
+    {
+        $categories->delete();
+        return redirect()->route('categories.index')->with('success',
+            'category deleted successfully.');
+    }
+}
---
+++ ProductsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = products::latest()->paginate(5);
+        return view('products.index',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'product_desc' => 'required',
+            'product_qty' => 'required',
+        ]);
+
+        products::create($request->all());
+        return redirect()->route('products.index')->with('success',
+            'Created Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Products $products)
+    {
+        return view('products.show',compact('products'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Products $products)
+    {
+        return view("products.edit",compact('products'));
+            }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Products $products)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'product_desc' => 'required',
+            'product_qty' => 'required',
+            'id_category' => 'required',
+        ]);
+
+        $products->update($request->all());
+        return redirect()->route('products.index')->with('success',
+            'Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Products $products)
+    {
+        $products->delete();
+        return redirect()->route('products.index')->with('success',
+            'Student deleted successfully.');
+    }
+}
